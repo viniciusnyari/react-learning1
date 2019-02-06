339aebdc88de0661a3e8910afb5c5bfc011fa616
Vinicius Adelir Nyari <viniciusnyari@gmail.com>
2019-02-06 09:20:27 -0200

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+
+  state = {
+      persons: [
+        {name: 'Vinícius', age:'37', gender:'Male'},
+        {name: 'Lilian', age:'41', gender:'Female'},
+        {name: 'Júlia', age:'4', gender:'Female'},
+      ]
+  }
+
+  //Método que reage por onClick 
+  switchNameHandler = (newName) => {
+    // console.log('Was Clicked!');
+    this.setState({
+      persons: [
+        // {name: 'Vinícius Nyari', age:'37', gender:'Male'},
+        {name: newName, age:'37', gender:'Male'},
+        {name: 'Lilian Cassia de Godoy Nyari', age:'41', gender:'Female'},
+        {name: 'Júlia de Godoy Nyari', age:'4', gender:'Female'},
+      ]
+    })
+  }
+
+  //Método que reage por Evento
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: 'Vinicius', age:'37', gender:'Male'},
+        {name: event.target.value, age:'41', gender:'Female'},
+        {name: 'Júlia', age:'4', gender:'Female'},
+      ]
+    })
+  }
+
   render() {
+
+    const style = {
+        backgroudColor:'white',
+        font:'inherit',
+        border : '1px solid black',
+        paddind: '8px',
+        cursor:'pointer'
+    } ; 
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Primeiro código em React.</h1>
+        <p>Veja! Isso funciona!</p>
+
+        <button style={style} onClick={this.switchNameHandler.bind(this,'Vnscs')}>Switch Name</button>
+        
+        <div>
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age} 
+            gender={this.state.persons[0].gender}>My hobbies: Soccer </Person>
+
+          <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age} 
+            gender={this.state.persons[1].gender}
+            click={this.switchNameHandler.bind(this,'VnscsLabs')}
+            changed = {this.nameChangeHandler}>My hobbies: Racing </Person>
+
+          <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age} 
+            gender={this.state.persons[2].gender}>My hobbies: Naná </Person> 
+
+        </div>
+
+        {/*
+        First Code...
+        <Person name="Vinícius" age="37" gender="Male">My hobbies: Soccer </Person>
+        <Person name="Lili" age="41" gender="Female">My hobbies: Racing </Person>
+        <Person name="Julia" age="4" gender="Female">My hobbies: Naná </Person>
+        */}
       </div>
     );
   }
---
+++ src/Person/Person.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Person.css';
+
+const person = (props) => {
+    return (
+        <div className="Person">
+            <p onClick={props.click}>I´m <strong>{props.name}</strong> and I am <strong>{props.age}</strong> years old and a <strong>{props.gender}</strong>!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
+        </div>
+    )
+};
+
+export default person;
